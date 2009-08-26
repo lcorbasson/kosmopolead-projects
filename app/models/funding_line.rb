b4726c2b82e9243981a91986e59d077c390d72b3
uneek-gaelle <gaelle@gaelle-uneek.(none)>
2009-08-26 17:20:39 +0200

    validation ligne de financement

--- app/models/funding_line.rb
+++ app/models/funding_line.rb
@@ -1,3 +1,6 @@
 class FundingLine < ActiveRecord::Base
+  
   belongs_to :project
+
+  validate_presence_of :aap,:financeur,:correspondant_financeur,:montant_demande, :type,:date_accord,:montant_accorde,:date_liberation,:montant_libere
 end

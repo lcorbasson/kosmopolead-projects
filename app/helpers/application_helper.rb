27b656d50ce38213e81e9917f6b2845a3fc421e8
uneek-gaelle <gaelle@gaelle-uneek.(none)>
2009-09-07 08:54:42 +0200

    correction bug scroll en haut de la page lors d'un toggle link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -72,8 +72,8 @@ module ApplicationHelper
 
   def toggle_link(name, id, options={})
     onclick = "Element.toggle('#{id}'); "
-    onclick << (options[:focus] ? "Form.Element.focus('#{options[:focus]}'); " : "this.blur(); ")
-    onclick << (options[:second_element] ?    "Element.toggle('#{options[:second_element]}');$('#{options[:second_element]}').blur();" : "")
+    onclick << (options[:focus] ? "Form.Element.focus('#{options[:focus]}'); " : "this.blur(); return false;")
+    onclick << (options[:second_element] ?    "Element.toggle('#{options[:second_element]}');$('#{options[:second_element]}').blur();return false;" : "")
     link_to(name, "#", :onclick => onclick,:class=>"button btn_orange corner-all")
   end
 

96a297d7baa95c4cdea798f55641082d154cf877
uneek-gaelle <gaelle@gaelle-uneek.(none)>
2009-09-08 10:43:43 +0200

    correction bug répertoire photos

--- config/initializers/uneek.rb
+++ config/initializers/uneek.rb
@@ -1,7 +1,3 @@
-class PaperclipUneek
-  DEFAULT_PHOTO = '/images/no-photo.png'
-  DEFAULT_LOGO_COMMUNITY = '/images/no-logo-community.png'
-  DEFAULT_LOGO_ORGANIZATION = '/images/no-logo-organization.png'
-  DEFAULT_LOGO_BRAND = '/images/no-logo-brand.png'
-  PAPERCLIP_URL = '/picts/:attachment/:id/:style_:basename.:extension'
+class PaperclipUneek  
+  PAPERCLIP_URL = '/picts/:attachment/:id/:style/:filename'
 end

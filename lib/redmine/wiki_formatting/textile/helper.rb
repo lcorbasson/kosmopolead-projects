d8faf753407eb4f0be9150b0de6a7ec89292bbec
uneek-gaelle <gaelle@gaelle-uneek.(none)>
2009-09-09 17:21:10 +0200

    correction bug affichage ie financement

--- lib/redmine/wiki_formatting/textile/helper.rb
+++ lib/redmine/wiki_formatting/textile/helper.rb
@@ -23,7 +23,7 @@ module Redmine
           help_link = l(:setting_text_formatting) + ': ' +
             link_to(l(:label_help), compute_public_path('wiki_syntax', 'help', 'html'),
                     :onclick => "window.open(\"#{ compute_public_path('wiki_syntax', 'help', 'html') }\", \"\", \"resizable=yes, location=no, width=300, height=640, menubar=no, status=no, scrollbars=yes\"); return false;")
-      
+                
           javascript_include_tag('jstoolbar/jstoolbar') +
             javascript_include_tag('jstoolbar/textile') +
             javascript_include_tag("jstoolbar/lang/jstoolbar-#{current_language}") +

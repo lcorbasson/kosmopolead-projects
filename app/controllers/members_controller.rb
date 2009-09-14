7aec1b9933aaf34ac5ce7405e46f404de171edb6
uneek-gaelle <gaelle@gaelle-uneek.(none)>
2009-09-14 18:12:28 +0200

    correction bug ajout partenaires

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -54,10 +54,7 @@ class MembersController < ApplicationController
 
         } }
       end   
-  end
-
-
-  
+  end  
 
   
   def edit
@@ -70,10 +67,10 @@ class MembersController < ApplicationController
   end
 
 
-
   def destroy
-    @member = Member.find(params[:id])
-    @member.destroy
+    member_to_destroy = Member.find(params[:id])
+    member_to_destroy.destroy
+    @member = Member.new
     respond_to do |format|
         format.js { render(:update) {|page|
             page.replace_html "projects_members", :partial => 'projects/show/members'

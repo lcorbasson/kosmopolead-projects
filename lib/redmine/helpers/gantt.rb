cc5c087e19349df7fc9e08c3649c3623ad77d18e
Zouko <zouko@Zouko.(none)>
2009-09-14 17:00:32 +0200

    centrage du gantt

--- lib/redmine/helpers/gantt.rb
+++ lib/redmine/helpers/gantt.rb
@@ -67,7 +67,12 @@ module Redmine
         @number_of_month = (@last_issue.year - @first_issue.year) * 12 + (@last_issue.month - @first_issue.month) + 1
 
         months = (options[:months]).to_i
-        @months = @number_of_month
+        if months > 0
+          @months = months
+        else
+          @months = @number_of_month
+        end
+        #@months =(months > 0 && months < 25) ? months : 6
         
         # Save gantt parameters as user preference (zoom and months count)
 #        if (User.current.logged? && (@zoom != User.current.pref[:gantt_zoom] || @months != User.current.pref[:gantt_months]))

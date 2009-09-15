2f73b40a3ea8ab0a5958d01685dbd0db6bad6e0e
Zouko <zouko@Zouko.(none)>
2009-09-15 14:36:25 +0200

    supression migrate

--- db/migrate/20090915121246_rename_sector_from_project.rb
+++
@@ -1,9 +0,0 @@
-class RenameSectorFromProject < ActiveRecord::Migration
-  def self.up
-    rename_column :projects,:sector_id, :sector
-  end
-
-  def self.down
-    rename_column :projects,:sector ,:sector_id
-  end
-end

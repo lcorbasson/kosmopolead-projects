52adc7218c7780aa23710bfef04ed5f81dea86f6
uneek-gaelle <gaelle@gaelle-uneek.(none)>
2009-09-17 09:26:12 +0200

    correction bug ajout du bloc demandes surveillées dans page accueil

--- config/initializers/uneek.rb
+++ config/initializers/uneek.rb
@@ -1,3 +1,5 @@
 class PaperclipUneek  
   PAPERCLIP_URL = '/picts/:attachment/:id/:style/:filename'
+  DEFAULT_LOGO_ORGANIZATION = '/images/no-logo-organization.png'
+  DEFAULT_PHOTO = '/images/no-photo.png'
 end

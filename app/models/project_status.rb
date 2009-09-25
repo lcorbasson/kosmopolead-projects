8a9f33a52cbc19593643af40e9bd8eb9d4776ee6
Emilien Taque <etaque@labo5.audencia.com>
2009-09-25 11:41:07 +0200

    ajout scope sur uniqueness

--- app/models/project_status.rb
+++ app/models/project_status.rb
@@ -6,7 +6,7 @@ class ProjectStatus < ActiveRecord::Base
   # -- validations
 
   validates_presence_of :status_label, :community
-  validates_uniqueness_of :status_label
+  validates_uniqueness_of :status_label, :scope => [:community_id]
   validates_length_of :status_label, :maximum => 30
   validates_format_of :status_label, :with => /^[\w\s\'\-]*$/i
 
